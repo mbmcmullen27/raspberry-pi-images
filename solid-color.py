dc5c14a3812da32b164928ec57af59f47b01f39d
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-01-31 20:11:13 -0600

    inner block

--- solid-color.py
+++ solid-color.py
@@ -13,7 +13,7 @@ tft_dc = board.D25
 displayio.release_displays()
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=board.D24)
 
-display = ST7789(display_bus, width=240, height=240, rowstart=20)
+display = ST7789(display_bus, width=280, height=240, rowstart=20, rotation=90)
 
 # Make the display context
 splash = displayio.Group()
@@ -21,12 +21,20 @@ display.show(splash)
 
 color_bitmap = displayio.Bitmap(280, 240, 1)
 color_palette = displayio.Palette(1)
-color_palette[0] = 0x00FF00
+color_palette[0] = 0xFF0000
 
 bg_sprite = displayio.TileGrid(color_bitmap,
                                pixel_shader=color_palette,
                                x=0, y=0)
 splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(240, 200, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0xAA0088  # Purple
+inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=20, y=20)
+splash.append(inner_sprite)
+
 
 while True:
     pass
