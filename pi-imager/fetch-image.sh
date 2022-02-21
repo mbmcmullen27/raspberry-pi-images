a36bfa11de7f87a22b1587d0009d6f85a5db0dc7
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-02-21 17:15:28 -0600

    formatting

--- pi-imager/fetch-image.sh
+++ pi-imager/fetch-image.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 url="https://downloads.raspberrypi.org/raspios_lite_armhf/images/"
 function fetch() {
     curl -s $1 |\
-        grep -Po $2 |\
+        grep -Po "(?<=href=\")$2" |\
         sort -nr |\
         head -n1
 } 
 
-img=$(fetch $url "(?<=href=\")raspios[^\"/]*")
-file=$(fetch $url$img/ "(?<=href=\")[^\./]*.zip")
+img=$(fetch $url "raspios[^\"/]*")
+file=$(fetch $url$img/ "[^\.]*.zip")
 
 echo "The latest image build is: $img"
 echo "Downloading $file"
