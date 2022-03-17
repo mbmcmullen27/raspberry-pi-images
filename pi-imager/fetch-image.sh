c68679667bd3fc06903d91faea9fb3c49524fbb8
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-03-16 22:57:10 -0500

    options

--- pi-imager/fetch-image.sh
+++ pi-imager/fetch-image.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
-
 url="https://downloads.raspberrypi.org/raspios_lite_armhf/images"
-function fetch() {
+
+function Fetch() {
     curl -s $1 |\
         grep -Po "(?<=href=\")$2" |\
         sort -nr |\
         head -n1
 } 
 
-img=$(fetch $url/ "raspios[^\"/]*")
-file=$(fetch $url/$img/ "[^\.]*.zip")
+img=$(Fetch $url/ "raspios[^\"/]*")
+file=$(Fetch $url/$img/ "[^\.]*.zip")
 
-echo "The latest image build is: $img"
+function Build() {
+  [ ! -d "./imgs" ] && mkdir "imgs"
+  sudo -E TMPDIR=/var/tmp packer build raspios.json
+}
 
-# wget $url/$img/$file -P imgs/
-image=$url/$img/$file
-jq ".builders[0].file_urls[0]=\"$image\" | .builders[0].file_checksum_url=\"${image}.sha256\"" \
-  packer-template.json > raspios.json
+function Download() {
+  wget $url/$img/$file -P imgs/
+}
 
-[ ! -d "./imgs" ] && mkdir "imgs"
+function Update() {
+  image=$url/$img/$file
+  jq ".builders[0].file_urls[0]=\"$image\" | .builders[0].file_checksum_url=\"${image}.sha256\"" \
+    packer-template.json > raspios.json
+}
 
-sudo -E TMPDIR=/var/tmp packer build raspios.json
+function Version() {
+  echo "The latest image build is: $img"
+}
+
+while getopts ":hbdv" option; do
+   case "${option}" in
+      h) # display Help
+        Help
+        exit;;
+      b) # build only don't fetch
+        Build 
+        exit;;
+      d) # download the latest image
+        Download
+        exit;;
+      u) # update template
+        Update
+        exit;;
+      v) # print current version
+        Version
+        exit;;
+      ?)
+        echo "unsupported option $1"
+        exit;;
+   esac
+done
+if [ "$#" -eq 0 ]; then
+  Update
+  Build
+  exit 0;
+fi
+
+
+
