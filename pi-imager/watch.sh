c4b3f1b893aa4d0b110f004e047160a081a22993
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-03-31 04:11:39 -0500

    add watch function

--- pi-imager/watch.sh
+++ pi-imager/watch.sh
@@ -5,22 +5,26 @@ DIR='sd'
 FOUND=false
 sudo dmesg -C
 
+function write() {
+  python write-lines.py $1 $2
+}
+
 while [ $FOUND == false ]; do
   disk=$(dmesg | egrep -o mmcblk[0-9])
   if [ -n "$disk" ]; then
     FOUND=true
-    python write-lines.py "sd card detected" "writing image..."  
+    write "sd card detected" "writing image..."  
     # mount /dev/$disk $DIR
     file=$(ls -1r imgs/ | head -n1)
     sudo dd if=imgs/$file of=/dev/$disk bs=4M conv=fsync
     # umount /dev/$disk
   else
     str="sleeping"
-    blankln='                    '
+    blankln=$(for i in {1..20}; do echo -n " "; done)
     for k in $(eval echo "{1..$it}"); do
         str+="."
     done
-    python write-lines.py "$str" "$blankln" 
+    write "$str" "$blankln" 
     sleep 7
   fi
   let it++
